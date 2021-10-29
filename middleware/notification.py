119677d8d6853c4b5789403572c325c298716e54
zwrig_000 <zwright14@gmail.com>
2021-10-29 15:59:35 -0500

    Added support for sending notification to SNS Topic

--- middleware/notification.py
+++ middleware/notification.py
@@ -1,8 +1,14 @@
+import boto3
+import json
+
 class Notification:
 
     def triggerNotification(path):
-        print("In Trigger")
-        print(path)
         pathList = ['/', '/orders']
         if path in pathList:
-            print("In PathList")
+            notification = "Get Event received for path: " + path
+            client = boto3.client('sns', region_name='us-east-1')
+            response = client.publish (
+            TargetArn = "arn:aws:sns:us-east-1:998678863102:GetEventTopic",
+            Message = json.dumps({'default': notification}), MessageStructure='json')
+            return {'statusCode': 200,'body': json.dumps(response)}
