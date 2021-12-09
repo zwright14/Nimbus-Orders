9a73b250a0b89b88f44943cfa15a10a680dff209
Zach Wright <zwright14@gmail.com>
2021-12-09 05:19:38 +0000

    Updated whitelist to include orders

--- middleware/security.py
+++ middleware/security.py
@@ -1,11 +1,13 @@
 from flask import Flask, Response, redirect, url_for
 from flask_dance.contrib.google import  google
 
+import re
+
 class Security:
 
     def checkLogin(path):
         # Create a whitelist of paths that don't need login access
-        loginWhitelist = ['/helloworld', '/userlogin/google','/userlogin/google/authorized']
-        if path not in loginWhitelist:
+        loginWhitelist = ['/helloworld', '/userlogin/google','/userlogin/google/authorized','/orders']
+        if path not in loginWhitelist and not re.match('\/orders\/\d+', path):
             if not google.authorized:
                 return redirect(url_for("google.login"))
